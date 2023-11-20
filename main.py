54541f598bcf9a8d8305b5bbdbfa02fafd62352d
gavrochexu <gavrochexu@gmail.com>
2023-11-20 19:53:10 +0800

    fix youtube api bug

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from utils import *
 
 
 def try_to_push_live_stream():
-    youtube_live_url = None
     schedule_start_time = None
 
     # main loop：
@@ -15,40 +14,47 @@ def try_to_push_live_stream():
     # 6. if both YouTube and twitch are not living, sleep 30 minutes and continue
     # 7. if repush failed, sleep 30s and continue
 
+    # check whether the channel is live streaming if youtube_live_url is not initialized
+    now = get_bj_time()
+    youtube_live_url = get_live_streaming_url()
+
+    # if the channel is live streaming, repush and return
+    if youtube_live_url is not None:
+        print(f"{now}: The channel is live streaming: {youtube_live_url}")
+        youtube_live_url = youtube_live_url
+        push_result = try_to_push_youtube(youtube_live_url) and try_to_push_twitch()
+
+        if push_result == 0:
+            print(f"{now}: Repush to kick successfully.")
+            time.sleep(60 * 60 * 12)
+            return
+
+        print(f"{now}: Repush youtube and twitch failed. Maybe caused by network, will retry 30s later.")
+        time.sleep(30)
+        return
+
     while True:
-        now = get_bj_time()
-
-        living_stream_url = get_live_streaming_url()
-
-        if living_stream_url is not None:
-            print(f"{now}: The channel is live streaming: {living_stream_url}")
-            youtube_live_url = living_stream_url
-            push_result = try_to_push_youtube(living_stream_url) and try_to_push_twitch()
-
-            if push_result == 0:
-                print(f"{now}: Repush to kick successfully.")
-                time.sleep(60 * 60 * 12)
-                return
-
-            print(f"{now}: Repush youtube and twitch failed. Maybe caused by network, will retry 30s later.")
-            time.sleep(30)
-            continue
-
+        # if the channel is not live streaming, check whether the youtube_live_url has get upcoming initialized
+        # if not try to get upcoming url
         if youtube_live_url is None and schedule_start_time is None:
             print(f"{now}: Try to get upcoming youtube live url...")
             res = get_upcoming_url()
             youtube_live_url = res[0]
             schedule_start_time = res[1]
 
+        # if the live stream is not upcoming, sleep 30 minutes and continue
         if youtube_live_url is None and schedule_start_time is None:
             print(f"{now}: Both youtube and twitch are not living or upcoming. Sleep 30 minutes.")
             time.sleep(30 * 60)
+            continue
 
+        # if the live stream is upcoming and not started yet, sleep 30s and continue
         if schedule_start_time and schedule_start_time > now:
             print(f"{now}: The live stream is not started yet.")
             time.sleep(30)
             continue
 
+        # if the live stream is upcoming and started, try to repush youtube and twitch
         push_result = try_to_push_youtube(youtube_live_url) and try_to_push_twitch()
         if push_result == 0:
             print(f"{now}: Repush twitch successfully.")
